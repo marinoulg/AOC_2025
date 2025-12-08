a943f6df76235cc1f8c6f0c5cfc1c55e46f8b9ff
marinelegal <m.legall75@gmail.com>
2025-12-08 23:24:43 +0100

    code propre

--- day_7.py
+++ day_7.py
@@ -5,16 +5,19 @@ my_input = open("example_day_7.txt", "r").read()
 new = my_input.split("\n")
 
 # print(new)
+# --------------- My functions --------------
 
-df = []
-for x in range(len(new)-1):
-    temp = []
-    for i in new[x]:
-        temp.append(i)
-    df.append(temp)
+def create_df(new):
+    df = []
+    for x in range(len(new)-1):
+        temp = []
+        for i in new[x]:
+            temp.append(i)
+        df.append(temp)
 
-df = (pd.DataFrame(df))
-print(df)
+    df = (pd.DataFrame(df))
+    # print(df)
+    return df
 
 def get_coordinates(df, to_find="@"):
     coordinates = []
@@ -24,24 +27,11 @@ def get_coordinates(df, to_find="@"):
                 coordinates.append((idx, col))
     return coordinates
 
-print()
-coordinates = get_coordinates(df, "^")
-print(coordinates)
-
-print()
-S = get_coordinates(df, "S")[0]
-
-# print()
-
 def when_encounter_truc(S, df):
     """
     S: 1 set of coordinates
     Ex: S = (x, y) or S = (idx, col)
     """
-    # new_S = [(S[0]+2, S[1]-1),
-    #          (S[0]+2, S[1]+1)]
-
-    # count = 0
 
     if df.loc[S] == "^":
         df.loc[S[0], S[1]+1] = "|"
@@ -55,31 +45,13 @@ def when_encounter_truc(S, df):
     else:
         pass
 
-    return df #, count #, new_S
+    return df
 
-# def when_encounter_truc_left(S, df):
-#     print(S)
-
-# count = 0
-for i in range(len(coordinates)):
-    A = (coordinates[i])
-    df = when_encounter_truc(A, df)
-    # count += c
-    # df=when_encounter_truc_right(A, df)
-
-# print(df.loc[A], A)
-# print(df.loc[A[0]+1, A[1]+1], (A[0]+1, A[1]+1))
-# print(df.loc[A[0]+1, A[0]-1], (A[0]+1, A[1]-1))
-
-print(df)
-# print()
-# print(count)
-# print(ns)
-
-trait_droit = get_coordinates(df, "|")
-
-trait = trait_droit[0]
-
+def surround_truc_with_traits(df, coordinates):
+    for i in range(len(coordinates)):
+        A = (coordinates[i])
+        df = when_encounter_truc(A, df)
+    return df
 
 def go_straight(trait, df, sign="|"):
     """
@@ -96,7 +68,7 @@ def go_straight(trait, df, sign="|"):
     # print("before condition:", (trait[0]+1, trait[1]))
     if df.loc[(trait[0]+1, trait[1])] != "^": #"== "." :
         # print("trait:", (trait[0]+1, trait[1]))
-        print((trait[0]+1, trait[1]))
+        # print((trait[0]+1, trait[1]))
         df.loc[(trait[0]+1, trait[1])] = sign
     else:
         # print("no")
@@ -105,35 +77,45 @@ def go_straight(trait, df, sign="|"):
     # print(df)
     return df
 
-for i in range(len(trait_droit)):
-    df = go_straight(trait_droit[i], df)
+def get_all_traits(df):
+    """
+    Looping '_' iterations in order to create the traits
+    if at (idx+1, col) != "^"
+    so that we create all the lines of the puzzle.
+    """
 
-print(df)
+    trait_droit = get_coordinates(df, "|")
+    max_lines = df.shape[0]-1
 
-print("we are here")
+    for _ in range(10):
+        temp = len(trait_droit)
+        for i in range(len(trait_droit)):
+            if (trait_droit[i][0] != max_lines): # or (coord[1] != df.shape[1]):
+                # c+=1
+                df = go_straight(trait_droit[i], df, "|")
+                # print(True)
+            # else:
+                # print(False)
 
-trait_droit = get_coordinates(df, "|")
+        trait_droit = get_coordinates(df, "|")
+        if len(trait_droit) == temp:
+            print(f"break at {_} iterations")
+            break
+    # print(df)
+    return df
 
-max_lines = df.shape[0]-1
 
-for i in range(len(trait_droit)):
-    if (trait_droit[i][0] != max_lines): # or (coord[1] != df.shape[1]):
-        # c+=1
-        df = go_straight(trait_droit[i], df, "|")
-        # print(True)
-    else:
-        print(False)
+# --------------- My code --------------
 
-trait_droit = get_coordinates(df, "|")
 
-max_lines = df.shape[0]-1
+df = create_df(new)
 
-for i in range(len(trait_droit)):
-    if (trait_droit[i][0] != max_lines): # or (coord[1] != df.shape[1]):
-        # c+=1
-        df = go_straight(trait_droit[i], df, "|")
-        # print(True)
-    else:
-        print(False)
+S = get_coordinates(df, "S")[0]
+df = go_straight(S, df)
+
+coordinates = get_coordinates(df, "^")
+
+df = surround_truc_with_traits(df, coordinates)
+df = get_all_traits(df)
 
 print(df)
