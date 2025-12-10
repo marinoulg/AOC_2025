4367019b4f1bb44d9330ccd37cb3c53a48b7a86f
marinelegal <m.legall75@gmail.com>
2025-12-11 00:54:31 +0100

    logic being implemented

--- day_8.py
+++ day_8.py
@@ -38,10 +38,10 @@ def compare_one_with_all(one, X_s, Y_s, Z_s):
             result = temp
             index = idx
 
-    print(f"({X_s[index]}, {Y_s[index]}, {Z_s[index]}) and ({X_s[-1]}, {Y_s[-1]}, {Z_s[-1]})")
+    # print(f"({X_s[index]}, {Y_s[index]}, {Z_s[index]}) and ({X_s[-1]}, {Y_s[-1]}, {Z_s[-1]})")
     return index, result
 
-def know_two_closest(X_s, Y_s, Z_s):
+def know_two_closest(X_s, Y_s, Z_s, is_not=None):
     order = []
     for idx in range(len(X_s)):
         index, result = (compare_one_with_all((X_s[idx], Y_s[idx], Z_s[idx]),
@@ -51,13 +51,26 @@ def know_two_closest(X_s, Y_s, Z_s):
     # to be able to easily compare, I create a df
     df = (pd.DataFrame(order).sort_values(by=0).drop(columns=1).rename(columns={0:"distances", 2:"coordinates"}).reset_index())
 
-    # I then find the two closest points
+    if is_not != None:
+        to_drop = []
+        for elem in is_not:
+            idx_to_be_dropped = list(df[df["distances"]==elem].index)[0]
+            to_drop.append(idx_to_be_dropped)
+        df.drop(index=to_drop, inplace=True)
+        df.reset_index(inplace=True)
+
     idx_compared_with_itself = (list(df.iloc[0:1,:]["index"])[0])
     other_idx = (list(df.iloc[1:2,:]["index"])[0])
+    res = df.loc[1,"distances"]
 
     # I drop the point in my df that is itself
     # df.drop(index=idx_compared_with_itself, inplace=True)
-    return idx_compared_with_itself, other_idx, df
+    return idx_compared_with_itself, other_idx, df, res
 
-idx_compared_with_itself, other_idx, df = know_two_closest(X_s, Y_s, Z_s)
-print(df)
+idx_compared_with_itself, other_idx, df, res = know_two_closest(X_s, Y_s, Z_s)
+
+to_drop = list()
+for _ in range(df.shape[0]):
+    idx_compared_with_itself, other_idx, df, res = know_two_closest(X_s, Y_s, Z_s, is_not=to_drop)
+    print(idx_compared_with_itself, other_idx)
+    to_drop.append(res)
