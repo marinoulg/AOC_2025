dcd4df0609cecd5e72058a3a15d62e16a09d38ee
marinelegal <m.legall75@gmail.com>
2025-12-28 17:26:11 +0100

    works on example, needs to be refactoed properly

--- day_8.py
+++ day_8.py
@@ -1,12 +1,34 @@
 import pandas as pd
 import numpy as np
+import networkx as nx
 
-my_input = open("example_day_8.txt", "r").read()
-new = my_input.split("\n")
+def get_all_tuples(text_file = "example_day_8.txt"):
+    """
+    this function creates a list of tuples from the given input.
+    it could be useful if we wanted to know the tuples that are alone after 9 iterations (eg, there are 7 in the provided example).
+    """
+    my_input = open(text_file, "r").read()
+    new = my_input.split("\n")[:-1]
 
-# print(new)
-# --------------- My functions --------------
-def create_lists_for_x_y_z(new):
+    all_tuples = []
+
+    for i in range(len(new)):
+        my_tuple = []
+        for elem in new[i].split(','):
+            my_tuple.append(int(elem))
+
+        all_tuples.append(tuple(my_tuple))
+    return all_tuples
+
+
+def create_lists_for_x_y_z(text_file="example_day_8.txt"):
+    """
+    this function creates separated lists for X, Y and Z
+    and allows for a DataFrame to be created
+    """
+    my_input = open(text_file, "r").read()
+    new = my_input.split("\n")[:-1]
+
     X_s = []
     Y_s = []
     Z_s = []
@@ -18,8 +40,14 @@ def create_lists_for_x_y_z(new):
     return X_s, Y_s, Z_s
 
 
-def compare_one_with_all(one_idx):
-    X_s, Y_s, Z_s = create_lists_for_x_y_z(new)
+def compare_one_with_all(one_idx, text_file="example_day_8.txt"):
+    """
+    this functions allows to compare distances between 1 cordinate and the others to know the closest one
+    - discontinued
+    """
+    my_input = open(text_file, "r").read()
+
+    X_s, Y_s, Z_s = create_lists_for_x_y_z(text_file)
     x_compared, y_compared, z_compared = X_s[one_idx], Y_s[one_idx], Z_s[one_idx]
 
     results = []
@@ -27,14 +55,15 @@ def compare_one_with_all(one_idx):
 
     result = int()
     index = int()
+    temps = {}
     for idx in range(len(X_s)):
         x3 = X_s[idx]
         y3 = Y_s[idx]
         z3 = Z_s[idx]
 
         temp = np.sqrt((x3-x_compared)**2+(y3-y_compared)**2+(z3-z_compared)**2)
-        # print(f"{idx}: {temp}")
-        # print()
+        temps[idx] = temp
+
         if idx == 0:
             result = temp
             index = idx
@@ -43,86 +72,178 @@ def compare_one_with_all(one_idx):
             index = idx
         results.append(temp)
         indexes.append(idx)
+    return index, result, temps # , results, indexes
 
-    # print(f"({X_s[index]}, {Y_s[index]}, {Z_s[index]}) and ({X_s[-1]}, {Y_s[-1]}, {Z_s[-1]})")
-    return index, result, results, indexes
 
-def know_two_closest(X_s, Y_s, Z_s, is_not=None):
-    order = []
+
+def get_ten_smallest_results(text_file="example_day_8.txt"):
+    """
+    this function creates the first DataFrame to be used in the knowledge graph.
+    """
+    my_input = open(text_file, "r").read()
+
+    X_s, Y_s, Z_s = create_lists_for_x_y_z(text_file)
+
+    df = pd.DataFrame()
+
     for idx in range(len(X_s)):
-        index, result = (compare_one_with_all((X_s[idx], Y_s[idx], Z_s[idx]),
-                                    X_s, Y_s, Z_s))
-        order.append((result, index, (X_s[idx], Y_s[idx], Z_s[idx])))
+        index, result, temps  = (compare_one_with_all(idx, text_file))
+        df_1 = pd.DataFrame([temps.keys(), temps.values()]).T
+        df_1["index"] = idx
+        df_1 = df_1.rename(columns={0:"compared_with", 1:"result"})
+        df_1 = df_1[["index", "compared_with", "result"]]
+        df_1
+        df = pd.concat([df, df_1])
 
-    # to be able to easily compare, I create a df
-    df = (pd.DataFrame(order).sort_values(by=0).drop(columns=1).rename(columns={0:"distances", 2:"coordinates"}).reset_index())
+    df.compared_with = df.compared_with.astype(int)
 
-    if is_not != None:
-        to_drop = []
-        for elem in is_not:
-            idx_to_be_dropped = list(df[df["distances"]==elem].index)[0]
-            to_drop.append(idx_to_be_dropped)
-        df.drop(index=to_drop, inplace=True)
-        df.reset_index(inplace=True)
+    df = df.sort_values(by="result")
+    df = df[df["result"] != 0]
 
-    idx_compared_with_itself = (list(df.iloc[0:1,:]["index"])[0])
-    other_idx = (list(df.iloc[1:2,:]["index"])[0])
-    res = df.loc[1,"distances"]
+    def get_coordinates_per_index(index):
+        x = X_s[index]
+        y = Y_s[index]
+        z = Z_s[index]
+        return (x,y,z)
 
-    # I drop the point in my df that is itself
-    # df.drop(index=idx_compared_with_itself, inplace=True)
-    return idx_compared_with_itself, other_idx, df, res
+    df["coords_index"] = df["index"].apply(get_coordinates_per_index)
+    df["coords_compared_with"] = df["compared_with"].apply(get_coordinates_per_index)
 
+    df = df.reset_index()
+    df = df.drop(columns="level_0")
 
-index_ = []
-compared_with_index = []
-result_ = []
-all_results = []
-X_s, Y_s, Z_s = create_lists_for_x_y_z(new)
-for idx in range(len(X_s)):
-    # print(idx)
-    index, result, results, indexes = (compare_one_with_all(idx))
-    # order.append((results, indexes, (X_s[idx], Y_s[idx], Z_s[idx])))
-    print("index:", index)
-    index_.append(index)
-    print("compared with index:",idx)
-    compared_with_index.append(idx)
-    print("result:", result)
-    result_.append(result)
-    print("results:", results)
-    all_results.append(results)
-    # print("all indexes:", indexes)
-    print()
+    print("ten smallest created")
 
-df = pd.DataFrame([index_, compared_with_index, result_, all_results])
-print()
-print(df)
+    return df
 
 
-# idx_compared_with_itself, other_idx, df, res = know_two_closest(X_s, Y_s, Z_s)
 
-# to_drop = list()
-# for _ in range(df.shape[0]):
-#     idx_compared_with_itself, other_idx, df, res = know_two_closest(X_s, Y_s, Z_s, is_not=to_drop)
-#     print(idx_compared_with_itself, other_idx)
-#     to_drop.append(res)
+def get_df_clean(text_file="example_day_8.txt"):
+    """
+    this function creates the final DataFrame to be used in the knowledge graph.
+    """
+    my_input = open(text_file, "r").read()
 
-# idx_compared_with_itself, other_idx, df, res = know_two_closest(X_s, Y_s, Z_s, is_not=to_drop)
-# print(idx_compared_with_itself, other_idx)
-# to_drop.append(res)
+    df = get_ten_smallest_results(text_file).head(40)
 
-# idx_compared_with_itself, other_idx, df, res = know_two_closest(X_s, Y_s, Z_s, is_not=to_drop)
-# print(idx_compared_with_itself, other_idx)
-# to_drop.append(res)
+    to_drop = []
+    for idx in range(df.shape[0]):
+        if idx%2==0:
+            to_drop.append(idx)
 
-# idx_compared_with_itself, other_idx, df, res = know_two_closest(X_s, Y_s, Z_s, is_not=to_drop)
-# print(idx_compared_with_itself, other_idx)
-# to_drop.append(res)
+    for idx in to_drop:
+        df.drop(index=idx, inplace=True)
 
-# idx_compared_with_itself, other_idx, df, res = know_two_closest(X_s, Y_s, Z_s, is_not=to_drop)
-# print(idx_compared_with_itself, other_idx)
-# to_drop.append(res)
+    df = df.reset_index().drop(columns="level_0")
 
-# idx_compared_with_itself, other_idx, df, res = know_two_closest(X_s, Y_s, Z_s, is_not=to_drop)
-# print(idx_compared_with_itself, other_idx)
-# to_drop.append(res)
+    print("df clean created")
+
+    return df
+
+
+def create_KG(text_file="example_day_8.txt"):
+    """
+    this function creates a graph to be used for the counting of the nodes and edges
+    """
+    df = get_df_clean(text_file)
+    # Create a knowledge graph
+    G = nx.Graph()
+    for _, row in df.iterrows():
+        G.add_edge(row['coords_index'], row['coords_compared_with'], label=row['result'])
+
+        if (_+1) == 9:
+            # Visualize the knowledge graph
+            pos = nx.spring_layout(G, seed=42, k=0.9)
+            labels = nx.get_edge_attributes(G, 'label')
+            # plt.figure(figsize=(12, 10))
+            # nx.draw(G, pos, with_labels=True, font_size=10, node_size=700, node_color='lightblue', edge_color='gray', alpha=0.6)
+            # nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=8, label_pos=0.3, verticalalignment='baseline')
+            # plt.title('Knowledge Graph')
+            # plt.show()
+            break
+
+    print("KG created")
+
+    return G
+
+def get_all_possible_connections(text_file="example_day_8.txt"):
+    """
+    this function creates a list of lists filled with tuples (x,y,z) of coordinates that are linked together,
+    either directly or indirectly
+    """
+    G = create_KG(text_file)
+
+    those_that_are_used = list(G.nodes)
+
+    connections = []
+    for key, value in dict(G.degree).items():
+        temp = []
+        if value > 1:
+            temp.append(key)
+            # print(key)
+            for node in those_that_are_used:
+                if G.has_edge(u=key,v=node):
+                    # print(node)
+                    if node not in temp:
+                        temp.append(node)
+            connections.append(temp)
+
+    # print(connections)
+
+    all_connections = []
+    temp_to_compare = []
+    for comparing in range(len(connections)):
+        conn_comparing = connections[comparing].copy()
+
+        for iterable in range(len(connections)):
+            conn_i = connections[iterable]
+
+            for i in range(len(conn_i)):
+                if conn_comparing[0] == conn_i[i]:
+                    for elem in (conn_i):
+                        if elem not in conn_comparing:
+                            conn_comparing.append(elem)
+
+        if sorted(conn_comparing) not in all_connections:
+            all_connections.append(sorted(conn_comparing))
+
+            for elem in conn_comparing:
+                temp_to_compare.append(elem)
+
+    for elem in list(dict(G.degree).keys()):
+        if elem not in temp_to_compare:
+            # print(sorted(list(sorted(list(G.edges(elem)))[0])))
+            sorting = sorted(list(sorted(list(G.edges(elem)))[0]))
+            if sorting not in all_connections:
+                all_connections.append(sorting)
+
+    print("all possible connections created")
+
+    return all_connections
+
+
+def answer_part_1(text_file="example_day_8.txt"):
+    """
+    this function prints out the result of the three biggest number of connections (direct or not)
+    within the graph we have built.
+    """
+    all_connections = get_all_possible_connections(text_file)
+
+    lengths = []
+    for i in range(len(all_connections)):
+        lengths.append(len(all_connections[i]))
+
+    print(lengths)
+
+    first = max(lengths)
+    lengths.remove(max(lengths))
+
+    second = max(lengths)
+    lengths.remove(max(lengths))
+
+    third = max(lengths)
+
+    return first * second * third
+
+print(answer_part_1("day_8.txt"))
+print(create_KG("day_8.txt"))
