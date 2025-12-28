50cc9f4abc8ef93465e1e8bdaf5990dcd87fa3f8
marinelegal <m.legall75@gmail.com>
2025-12-28 01:23:16 +0100

    no need for dataframe bc i was not using it - part 1 done

--- day_9.py
+++ day_9.py
@@ -14,43 +14,35 @@ def create_df(text_file="example_day_9.txt"):
 
     biggest_x = pd.DataFrame(coordinates).sort_values(by=0, ascending=False).reset_index().loc[0,0]
     biggest_y = pd.DataFrame(coordinates).sort_values(by=0, ascending=False).reset_index().loc[0,1]
-
-    return coordinates, biggest_x, biggest_y
-
-def answer_part_1(text_file="example_day_9.txt"):
-    coordinates, biggest_x, biggest_y = create_df(text_file)
     if biggest_x > biggest_y:
         biggest = biggest_x + 1
     else:
         biggest = biggest_y + 1
 
-    df = pd.DataFrame([['.']*(biggest_y+2)]*(biggest+1)).T
-
     for idx in coordinates:
         x,y = idx
-        df.loc[x,y] = "#"
 
-    df.index = list(range(1,df.shape[0]+1))
-    df.columns = list(range(1,df.shape[1]+1))
-    print("shape:",df.shape)
     print("coordinates",coordinates)
     print("length of coordinates:", len(coordinates))
 
-    """
-    seconde partie de l'exercice
-    --> ça bloque ici car pas assez de RAM
-    """
+    print("first part done")
+    return coordinates, biggest_x, biggest_y
+
+def answer_part_1(text_file="example_day_9.txt"):
+    coordinates, biggest_x, biggest_y = create_df(text_file)
+
     big = int()
     length = int(len(coordinates)/2)+1
     for idx in range(length):
+        print("iteration n°", idx)
         x_a, y_a = coordinates[idx]
-        print((x_a, y_a))
+        # print((x_a, y_a))
 
         for i in range(len(coordinates)):
             x_b, y_b = coordinates[i]
 
             if x_a != x_b and y_a != y_b:
-                print(f"{(x_a, y_a)} and {(x_b, y_b )}", end=": ")
+                # print(f"{(x_a, y_a)} and {(x_b, y_b )}", end=": ")
 
                 longueur = abs((x_a+1)-(x_b+1))+1
                 largeur = abs((y_a+1)-(y_b+1))+1
@@ -59,8 +51,9 @@ def answer_part_1(text_file="example_day_9.txt"):
                 # areas.append(
                 #     ((longueur * largeur), \
                 #                 (x_a, y_a), (x_b, y_b)))
-                print((longueur, largeur))
-        print()
+                # print((longueur, largeur))
+
+        # print()
 
     print(big)
 
