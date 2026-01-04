852978dcd8a513b60909e7399b439f2d29808ae8
marine le gall <marine.legall@alumni.esade.edu>
2026-01-04 15:19:12 +0100

    **

--- day_2.py
+++ day_2.py
@@ -1,47 +1,46 @@
-my_input = open("day_2.txt", "r").read()
+# ------------- Part 1 -------------
 
-updated_input = my_input.split(",")
-
-# Part 1
-"""
 def is_invalid_1(x):
-    if len(str(x)) % 2 == 0: # length is even
+    if len(str(x)) % 2 == 0:
             half = int(len(str(x))/2)
 
             first_half = str(x)[:half]
             last_half = str(x)[half:]
             if first_half == last_half:
                 invalid_ID = int(first_half+last_half)
-                # print(invalid_ID)
                 return invalid_ID
 
-invalid_IDs = []
-for elem in updated_input:
-    start, end = elem.split("-")
-    start = int(start)
-    end = int(end)
-    # print("start is:", start)
-    # print("end is:", end)
+def answer_part1(text_file="day_2.txt"):
+    my_input = open(text_file, "r").read()
+    updated_input = my_input.split(",")
 
-    for x in range(start, end+1):
-        if is_invalid_1(x):
-            invalid_ID = is_invalid_1(x)
-            invalid_IDs.append(invalid_ID)
-"""
-# Part 2
+    invalid_IDs = []
+    for elem in updated_input:
+        start, end = elem.split("-")
+        start = int(start)
+        end = int(end)
+
+        for x in range(start, end+1):
+            if is_invalid_1(x):
+                invalid_ID = is_invalid_1(x)
+                invalid_IDs.append(invalid_ID)
+    return (sum(invalid_IDs))
+
+print("Part 1:", answer_part1())
+
+# ------------- Part 2 -------------
 
 def is_invalid(x):
-    if len(str(x)) % 2 == 0: # length is even
+    if len(str(x)) % 2 == 0:
             half = int(len(str(x))/2)
 
             first_half = str(x)[:half]
             last_half = str(x)[half:]
             if first_half == last_half:
                 invalid_ID = int(first_half+last_half)
-                # print(invalid_ID)
                 return invalid_ID
 
-    if len(str(x)) % 3 == 0: # length is divisible by 3
+    if len(str(x)) % 3 == 0:
         div = int(len(str(x))/3)
 
         first_div = str(x)[:div]
@@ -49,11 +48,9 @@ def is_invalid(x):
         third_div = str(x)[2*div:]
         if first_div == sec_div == third_div:
             invalid_ID = int(first_div+sec_div+third_div)
-            # print(invalid_ID)
             return invalid_ID
-            # invalid_IDs.append(invalid_ID)
 
-    if len(str(x)) % 5 == 0: # length is divisible by 5
+    if len(str(x)) % 5 == 0:
         div = int(len(str(x))/5)
 
         first_div = str(x)[:div]
@@ -64,11 +61,9 @@ def is_invalid(x):
 
         if first_div == sec_div == third_div == fourth_div == fifth_div:
             invalid_ID = int(first_div+sec_div+third_div+fourth_div+fifth_div)
-            # print(invalid_ID)
-            # invalid_IDs.append(invalid_ID)
             return invalid_ID
 
-    if len(str(x)) % 7 == 0: # length is divisible by 7
+    if len(str(x)) % 7 == 0:
         div = int(len(str(x))/7)
 
         first_div = str(x)[:div]
@@ -80,23 +75,24 @@ def is_invalid(x):
         seventh_div = str(x)[6*div:]
 
         if first_div == sec_div == third_div == fourth_div == fifth_div == sixth_div == seventh_div:
-            invalid_ID = int(first_div+sec_div+third_div+fourth_div+fifth_div+sixth_div+seventh_div)
-            # print(invalid_ID)
-            # invalid_IDs.append(invalid_ID)
+            invalid_ID = int(first_div + sec_div + third_div + fourth_div + fifth_div + sixth_div + seventh_div)
             return invalid_ID
 
-invalid_IDs = []
-for elem in updated_input:
-    start, end = elem.split("-")
-    start = int(start)
-    end = int(end)
-    # print("start is:", start)
-    # print("end is:", end)
+def answer_part2(text_file="day_2.txt"):
+    my_input = open(text_file, "r").read()
+    updated_input = my_input.split(",")
 
-    for x in range(start, end+1):
-        if is_invalid(x):
-            invalid_ID = is_invalid(x)
-            invalid_IDs.append(invalid_ID)
+    invalid_IDs = []
+    for elem in updated_input:
+        start, end = elem.split("-")
+        start = int(start)
+        end = int(end)
 
-print(invalid_IDs)
-print(sum(invalid_IDs))
+        for x in range(start, end+1):
+            if is_invalid(x):
+                invalid_ID = is_invalid(x)
+                invalid_IDs.append(invalid_ID)
+
+    return (sum(invalid_IDs))
+
+print("Part 2:", answer_part2())
